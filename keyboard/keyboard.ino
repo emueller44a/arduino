ecf5d1ecefe8abc7fa6976fe7dd461cb6cffc072
Raphael Zeiger <panic@vor.spam>
2016-12-03 16:07:18 +0100

    serial count

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -1,3 +1,19 @@
 /*
  * test
  */ 
+ 
+ int i = 1;
+ 
+ void setup()
+ {
+   Serial.begin(9600);
+   while (!Serial) {
+   }
+ }
+ 
+ void loop()
+ {
+   Serial.println(i);
+   delay(1000);
+   i++;
+ }
